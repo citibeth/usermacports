6775c4b0581439a3e56ed624cd0b22f4566d94fe
citibeth <rpf2116@columbia.edu>
2016-01-09 23:59:06 -0500

    cmakedev now produces a script to be used to call CMake.

--- ebx/pylib/easybuild/easyblocks/cmakedev.py
+++ ebx/pylib/easybuild/easyblocks/cmakedev.py
@@ -6,11 +6,22 @@ software instead of a full install via EasyBuild.
 """
 import os
 import re
+import stat
 
 from easybuild.easyblocks.generic.cmakemake import CMakeMake
 from easybuild.tools.environment import setvar
 from easybuild.tools.run import run_cmd
 import easybuild.tools.environment as env
+
+def is_exe(fpath):
+	return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+def which(program):
+	for path in os.getenv("PATH").split(os.pathsep):
+		path = path.strip('"')
+		exe_file = os.path.join(path, program)
+		if is_exe(exe_file):
+			return exe_file
 
 class CmakeDev(CMakeMake):
 	"""
@@ -59,11 +70,6 @@ class CmakeDev(CMakeMake):
 		"""
 		super(CmakeDev, self).prepare_step(*args, **kwargs)
 
-
-#	def configure_step(self, *args, **kwargs):
-#		self.builddir = self.src[0]['path']
-
-
 	def make_installdir(self, dontcreate=None):
 		"""Custom implementation of make installdir: do nothing, do
 		not touch system compiler directories and files."""
@@ -76,61 +82,124 @@ class CmakeDev(CMakeMake):
 		if builddir is not None:
 			self.log.nosupport("CMakeMake.configure_step: named argument 'builddir' (should be 'srcdir')", "2.0")
 
-		# Set the search paths for CMake
-		include_paths = os.pathsep.join(self.toolchain.get_variable("CPPFLAGS", list))
-		library_paths = os.pathsep.join(self.toolchain.get_variable("LDFLAGS", list))
-		setvar("CMAKE_INCLUDE_PATH", include_paths)
-		setvar("CMAKE_LIBRARY_PATH", library_paths)
-
 		default_srcdir = '.'
-                separate_build_dir = True	# Always do this option; it make such a mess if you don't
-		# separate_build_dir = self.cfg.get('separate_build_dir', False)
-		if separate_build_dir:
-			objdir = os.path.join(self.builddir, 'build')
-			self.log.info("Creating CMake build dir %s" % objdir)
-			try:
-				os.mkdir(objdir)
-			except OSError, err:
-				pass
 
-			try:
-				os.chdir(objdir)
-			except OSError, err:
-				raise EasyBuildError("Failed to cd to separate build dir %s in %s: %s", objdir, os.getcwd(), err)
-			default_srcdir = self.cfg['start_dir']
+		# if separate_build_dir...
+		objdir = os.path.join(self.builddir, 'build')
+		configme_sh = os.path.join(self.builddir, 'ebconfig.py')
+		configme_out = open(configme_sh, 'w')
+		configme_out.write(
+r"""#!/usr/bin/env python
+#
+
+import os
+import subprocess
+
+def cmdlist(str):
+	return list(x.strip().replace("'",'') for x in str.split('\n') if x)
+
+env = dict()
+""")
+
+		self.log.info("Creating CMake build dir %s" % objdir)
+		try:
+			os.mkdir(objdir)
+		except OSError, err:
+			pass
+
+		try:
+			os.chdir(objdir)
+		except OSError, err:
+			raise EasyBuildError("Failed to cd to separate build dir %s in %s: %s", objdir, os.getcwd(), err)
+		default_srcdir = self.cfg['start_dir']
 
 		if srcdir is None:
 			if self.cfg.get('srcdir', None) is not None:
 				srcdir = self.cfg['srcdir']
 			else:
 				srcdir = default_srcdir
+		# -----------------
+
+		# Set the search paths for CMake
+		include_paths = os.pathsep.join(self.toolchain.get_variable("CPPFLAGS", list))
+		library_paths = os.pathsep.join(self.toolchain.get_variable("LDFLAGS", list))
+		setvar("CMAKE_INCLUDE_PATH", include_paths)
+		setvar("CMAKE_LIBRARY_PATH", library_paths)
+
+		configme_out.write("env['CMAKE_INCLUDE_PATH'] = os.pathsep.join(cmdlist(")
+		configme_out.write('"""\n')
+		for path in self.toolchain.get_variable("CPPFLAGS", list):
+			configme_out.write("    %s\n" % path)
+		configme_out.write('"""')
+		configme_out.write("))\n\n")
+
+		configme_out.write("env['CMAKE_LIBRARY_PATH'] = os.pathsep.join(cmdlist(")
+		configme_out.write('"""\n')
+		for path in self.toolchain.get_variable("LDFLAGS", list):
+			configme_out.write("    %s\n" % path)
+		configme_out.write('"""')
+		configme_out.write("))\n\n")
+
+
+#		configme_out.write("export CMAKE_INCLUDE_PATH='%s'\n" % include_paths)
+#		configme_out.write("export CMAKE_LIBRARY_PATH='%s'\n" % library_paths)
 
 		options = ['-DCMAKE_INSTALL_PREFIX=%s' % self.installdir]
-		env_to_options = {
-			'CC': 'CMAKE_C_COMPILER',
-			'CFLAGS': 'CMAKE_C_FLAGS',
-			'CXX': 'CMAKE_CXX_COMPILER',
-			'CXXFLAGS': 'CMAKE_CXX_FLAGS',
-			'F90': 'CMAKE_Fortran_COMPILER',
-			'FFLAGS': 'CMAKE_Fortran_FLAGS',
-		}
-		for env_name, option in env_to_options.items():
-			value = os.getenv(env_name)
-			if value is not None:
-				options.append("-D%s='%s'" % (option, value))
+
+		tcvar = self.toolchain.get_variable
+		options.append('-DCMAKE_C_COMPILER=%s'       % which(tcvar('CC')))
+		options.append('-DCMAKE_C_FLAGS=%s'          % tcvar('CFLAGS'))
+		options.append('-DCMAKE_CXX_COMPILER=%s'     % which(tcvar('CXX')))
+		options.append('-DCMAKE_CXX_FLAGS=%s'        % tcvar('CXXFLAGS'))
+		options.append('-DCMAKE_Fortran_COMPILER=%s' % which(tcvar('F90')))
+		options.append('-DCMAKE_Fortran_FLAGS=%s'    % tcvar('F90FLAGS'))
+
+
+#		env_to_options = {
+#			'CC': 'CMAKE_C_COMPILER',
+#			'CFLAGS': 'CMAKE_C_FLAGS',
+#			'CXX': 'CMAKE_CXX_COMPILER',
+#			'CXXFLAGS': 'CMAKE_CXX_FLAGS',
+#			'F90': 'CMAKE_Fortran_COMPILER',
+#			'FFLAGS': 'CMAKE_Fortran_FLAGS',
+#		}
+#		for env_name, option in env_to_options.items():
+#			value = os.getenv(env_name)
+#			if value is not None:
+#				options.append("-D%s='%s'" % (option, value))
+
 
 		# show what CMake is doing by default
-		options.append("-DCMAKE_VERBOSE_MAKEFILE=ON")
+#		options.append("-DCMAKE_VERBOSE_MAKEFILE=ON")
 
 		options_string = " ".join(options)
+
+		configme_out.write('cmd = cmdlist("""\n')
+		configme_out.write('%s\n' % which('cmake'))
+		configme_out.write('    %s\n' % srcdir)
+		for opt in options:
+			configme_out.write('    %s\n' % opt)
+		configme_out.write('""")\n\n')
+		configme_out.write
+		configme_out.write("proc = subprocess.Popen(cmd, env=env, cwd=%s)\n" % repr(objdir))
+		configme_out.write("proc.wait()\n")
+
+		configme_out.close()
+		os.chmod(configme_sh, os.stat(configme_sh).st_mode | stat.S_IEXEC)
+
+
 
 		command = "%s cmake %s %s %s" % (self.cfg['preconfigopts'], srcdir, options_string, self.cfg['configopts'])
 		self.log.info('CWD = %s' % os.getcwd())
 		self.log.info('Running cmd: %s' % command)
-		(out, _) = run_cmd(command, log_all=True, simple=False)
 
-		return out
 
+#		(out, _) = run_cmd(command, log_all=True, simple=False)
+#		return out
+		return 0
+
+	def buid_step(self, *args, **kwargs):
+		pass
 
 	def cleanup_step(self):
 		"""Do NOT remove the build directory, under any circumstances!"""
